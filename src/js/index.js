0f4daf0c9e97c0bcf6c6873cca71c5bcd076463f
codergon <atakerewilliams@gmail.com>
2024-12-01 14:15:39 +0100

    using same container

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,94 @@
+import gsap from "gsap";
+import Component from "./classes/Component";
+import { Flip } from "gsap/all";
 
+gsap.registerPlugin(Flip);
+
+const cards = new Component({
+  element: "body",
+  elements: {
+    cards: ".card",
+  },
+});
+
+export default class Home {
+  constructor() {
+    this.cards = cards.elements.cards;
+
+    // this.setup();
+    this.addListeners();
+  }
+
+  setup() {
+    const tl = gsap.timeline();
+
+    tl.from(".home__topbar", {
+      delay: 0.3,
+      opacity: 0,
+      duration: 0.5,
+      yPercent: -100,
+      ease: "power2.out",
+    })
+      .from(this.cards, {
+        duration: 0.6,
+        ease: "power2.out",
+        clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
+        stagger: {
+          amount: 0.15,
+        },
+      })
+      .from(
+        ".home__nav",
+        {
+          y: "80%",
+          opacity: 0,
+          duration: 0.5,
+          ease: "power2.out",
+        },
+        "<+=0.2"
+      );
+  }
+
+  flip(element) {
+    const content = document.querySelector(".home__content");
+
+    const state = Flip.getState(".card");
+    const flipId = element.dataset.flipId;
+
+    content.classList.add("preview");
+    content.querySelectorAll(".card").forEach((card) => {
+      if (card.dataset.flipId !== flipId) {
+        card.classList.add("hidden");
+      }
+    });
+
+    Flip.from(state, {
+      paused: true,
+      duration: 0.6,
+      absolute: true,
+      ease: "power2.out",
+      onLeave: (elements) => {
+        console.log(elements);
+
+        gsap.to(elements, {
+          duration: 0.6,
+          ease: "power2.out",
+          clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
+          stagger: {
+            amount: 0.15,
+          },
+        });
+      },
+    });
+  }
+
+  addListeners() {
+    this.cards.forEach((card) => {
+      card.addEventListener("click", () => {
+        this.flip(card);
+      });
+    });
+  }
+}
+
+new Home();
